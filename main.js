8e699267470a14a50c768e41b05963f3887c884c
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-06-04 22:41:45 +0000

    Create While Loop version of function

--- main.js
+++ main.js
@@ -9,4 +9,23 @@ function findCumulativeSum(number) {
     return sum;
 }
 
-console.log(findCumulativeSum(5))
+console.log(findCumulativeSum(10))
+
+// While Loop form (for practice)
+
+function findCumulativeSum2(number) {
+    let sum = 0
+    let i = 0
+
+    while (i <= number) {
+        sum += i;
+        i++;
+    }
+    
+    return sum;
+}
+
+console.log(findCumulativeSum2(10))
+
+// Can also write both without console logging the function and instead changing the return sum lines to 
+// console.log(sum) and just directly invoking the function outside of the function declaration
