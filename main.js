9ab43da31ff38499e7bba0fbc88041e3838a035a
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-06-04 22:19:47 +0000

    Create findCumulativeSum(number) function for all positive integers

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // Write your code below
+function findCumulativeSum(number) {
+    let sum = 0;
+    
+    for (let i = 0; i <= number; i++) {
+        sum += i;
+    }
+    
+    return sum;
+}
+
+console.log(findCumulativeSum(500))
